f3bd41f480a2b2ce175b5faf589d113686ee4968
Hein Htut Aung <heinhtutaung@Heins-iMac.local>
2024-06-26 00:42:30 +0630

    First Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const textInput = document.getElementById("text-input");
+const checkBtn = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
+function cleanInputString(str) {
+    const regex = /[\s\W_]/gi;
+    return str.replace(regex, '');
+}
+
+function clickCheckBtn(input) {
+    let text = cleanInputString(input);
+    if(text === "") {
+        alert("Please input a value."); 
+        return;
+    }
+    if(text.toLowerCase() === text.toLowerCase().split("").reverse().join("")) {
+        result.textContent = `${input} is a palindrome.`;
+    } else {
+        result.textContent = `${input} is not a palindrome.`;
+    }
+}
+
+checkBtn.addEventListener("click", () => {
+    clickCheckBtn(textInput.value);
+});
